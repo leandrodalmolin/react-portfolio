54e31538b159c048bc9ec656e39497ee205815ad
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-10-08 15:42:40 +0100

    Add lazy loading to images and decrease its sizes

--- src/components/sections/Work.jsx
+++ src/components/sections/Work.jsx
@@ -1,10 +1,15 @@
 import { Card } from "../ui/Card";
 
 import rlImage from "../../assets/images/work/rl-work.jpg";
+import rlPhImage from "../../assets/images/work/lowres/rl-work.jpg";
 import sdImage from "../../assets/images/work/sd-work.jpg";
+import sdPhImage from "../../assets/images/work/lowres/sd-work.jpg";
 import gwImage from "../../assets/images/work/gw-work.jpg";
+import gwPhImage from "../../assets/images/work/lowres/gw-work.jpg";
 import cgImage from "../../assets/images/work/cf-work.jpg";
+import cgPhImage from "../../assets/images/work/lowres/cf-work.jpg";
 import spcImage from "../../assets/images/work/spc-work.jpg";
+import spcPhImage from "../../assets/images/work/lowres/spc-work.jpg";
 import { Wrapper } from "../abstracts/Wrapper";
 import { Section } from "../abstracts/Section";
 import { Heading } from "../ui/Heading";
@@ -17,6 +22,7 @@ const WORK_DATA = [
       "GolfWorking specialise in building websites for golf clubs, with clients throughout UK and Ireland.",
     url: "https://www.golfworking.co.uk/",
     imageSrc: gwImage,
+    placeholderImageSrc: gwPhImage,
     bgColor: "bg-[#222]",
   },
   {
@@ -33,6 +39,7 @@ const WORK_DATA = [
       "Established in 1894, the Harlech links has long been ranked within the ‘Top Fifty’ of British golf courses, and most recently ranked 2nd in the Top 50 Courses in Wales by Golf World.",
     url: "https://www.royalstdavids.co.uk/",
     imageSrc: sdImage,
+    placeholderImageSrc: sdPhImage,
     bgColor: "bg-[#2f3543]",
   },
   {
@@ -41,6 +48,7 @@ const WORK_DATA = [
       "Congo Falls Adventure Golf is a spectacular outdoor mini golf course suitable for adults of all ages and families.",
     url: "https://www.congofalls.co.uk/",
     imageSrc: cgImage,
+    placeholderImageSrc: cgPhImage,
     bgColor: "bg-[#29452A]",
   },
   {
@@ -49,6 +57,7 @@ const WORK_DATA = [
       "Leading global manufacturer of rubber compounds, specialise in technically demanding high quality materials used in seals, gaskets, hoses and more.",
     url: "https://www.spc-group.com/",
     imageSrc: spcImage,
+    placeholderImageSrc: spcPhImage,
     bgColor: "bg-[#002856]",
   },
 ];
@@ -79,6 +88,7 @@ export function Work() {
                       description={work.description}
                       url={work.url}
                       imageSrc={work.imageSrc}
+                      placeholderImageSrc={work.placeholderImageSrc}
                     />
                   </FadeInScroll>
                 </div>
--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,18 +1,23 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "./Link";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 export function Card(props) {
-  const { title, description, url, imageSrc, extraClasses } = props;
+  const { title, description, url, imageSrc, placeholderImageSrc, extraClasses } = props;
 
   return (
-    <div className={`group text-left px-[10%] pt-[10%] md:pt-[8%] pb-0 bg-zinc-800 ${extraClasses ?? ""}`}>
-      <h2 className="mb-4 md:mb-7 text-2xl text-white font-bold">
-        {title}
-      </h2>
+    <div
+      className={`group text-left px-[10%] pt-[10%] md:pt-[8%] pb-0 bg-zinc-800 ${
+        extraClasses ?? ""
+      }`}
+    >
+      <h2 className="mb-4 md:mb-7 text-2xl text-white font-bold">{title}</h2>
       <div className="md:duration-[1.5s] md:ease-[cubic-bezier(0.65, 0, 0.35, 1)] md:transition-all md:invisible md:max-h-0 md:opacity-0 group-hover:md:visible group-hover:md:max-h-[800px] group-hover:md:opacity-100">
-        <p className="mb-6 text-white/60 text-sm leading-relaxed">{description}</p>
+        <p className="mb-6 text-white/60 text-sm leading-relaxed">
+          {description}
+        </p>
         <p className="text-sm">
-          <Link 
+          <Link
             extraClasses="flex inline-flex items-center gap-2 border-b-2 border-white/20 text-white/60"
             href={url}
           >
@@ -22,7 +27,12 @@ export function Card(props) {
         </p>
       </div>
       <div className="mt-10 md:mt-0 group-hover:md:mt-10 md:ease-[cubic-bezier(0.65, 0, 0.35, 1)] md:duration-[1.5s] md:transition-[margin] border-white/10 border-x-8 border-t-8 rounded-tl-md rounded-tr-md overflow-hidden">
-        <img className="block" src={imageSrc} alt={title} />
+        <LazyLoadImage
+          className="block"
+          src={imageSrc}
+          placeholderSrc={placeholderImageSrc}
+          alt={title}
+        />
       </div>
     </div>
   );
