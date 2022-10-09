0c145f9c4928b33a7baa840a402ae940d15ceafc
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-10-09 19:28:56 +0100

    Separate styles by type in the Link component

--- src/components/ui/Link.jsx
+++ src/components/ui/Link.jsx
@@ -1,6 +1,8 @@
 export function Link(props) {
   const { type = "default", extraClasses, ...rest } = props;
-  let variationClasses = "";
+  
+  // Default
+  let variationClasses = "hover:text-white theme-light:hover:text-zinc-900";
 
   if (type === "border") {
     variationClasses = "font-bold border-2 text-zinc-300 border-white/20 rounded-md text-sm px-6 py-2 sm:text-base sm:px-5 hover:border-white hover:bg-white hover:text-zinc-900";
@@ -8,7 +10,7 @@ export function Link(props) {
 
   return (
     <a
-      className={`hover:text-white theme-light:hover:text-zinc-900 duration-300 transition-colors ${variationClasses} ${extraClasses ?? ""}`}
+      className={`duration-300 transition-colors ${variationClasses} ${extraClasses ?? ""}`}
       {...rest}
     >
       {props.children}
